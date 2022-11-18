914e21f453a7cd725469fd70d32b68eb072e5dea
Ilkham <ilham992@mail.ru>
2022-11-18 12:41:41 +0400

    change route

--- README.md
+++ README.md
@@ -7,5 +7,3 @@
     npm start
 ```
 and open [http://localhost:3000/alpha-test](http://localhost:3000/alpha-test)
-
-
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom';
 import './header.styles.scss';
 
 
 export const Header = () => (
   <header className='header'>
     <div className='container'>
-      <a href='/' className='header__title'>
+      <Link to='/' className='header__title'>
         <h1>Тестовое задание</h1>
-      </a>
+      </Link>
     </div>
   </header>
 )
